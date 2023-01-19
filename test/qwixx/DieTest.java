1a9986ecb37e8048fc3f2b70568fe9ca4b7ea4df
goober <gabriel@lclark.edu>
2023-01-19 15:25:19 -0800

    fixed tests

--- test/qwixx/DieTest.java
+++ test/qwixx/DieTest.java
@@ -1,13 +1,14 @@
-package qwixxTests;
+package qwixx;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import qwixx.Die;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DieTest {
+
     private Die d;
+
     @BeforeEach
     void setUp(){
         d = new Die();
@@ -15,21 +16,19 @@ class DieTest {
 
     @Test
     void rolledNumberIsBetween1and6() {
-
         d.roll();
         assertTrue(d.getValue() >= 1);
-        assertTrue(d.getValue() >= 6);
+        assertTrue(d.getValue() <= 6);
     }
 
     @Test
-    void all6NumbersEaullyLikely(){
-        d = new Die();
+    void all6NumbersEquallyLikely(){
         int[] counts = new int[7];
         for (int i = 0; i < 600; i++) {
             d.roll();
             counts[d.getValue()]++;
         }
-        for (int i = 0; i <= 6; i++) {
+        for (int i = 1; i <= 6; i++) {
             assertTrue(counts[i]>50);
             assertTrue(counts[i]<150);
 

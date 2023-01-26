d9cee500a078803bf4b352378a43c78d57693cff
1dylanangel <1dylanangel@gmail.com>
2023-01-26 15:26:24 -0800

    Added scanner to check number of desired players as well as take in player names and insert into map

--- src/qwixx/Qwixx.java
+++ src/qwixx/Qwixx.java
@@ -1,32 +1,21 @@
 package qwixx;
 
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class Qwixx {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String name1= scan.nextLine();
-        String name2= scan.nextLine();
+        LinkedHashMap<String, DisplayCard> players = new LinkedHashMap<>();
+        int numPlayers = scan.nextInt();
+        for (int i = 0; i <= numPlayers; ++i) {
+            String name = scan.nextLine();
+            players.put(name, new DisplayCard(name));
+        }
 
+        for (DisplayCard playerCard: players.values()) {
+            playerCard.displayPlayerCard();
+        }
 
-        DisplayCard playerOne = new DisplayCard(name1);
-        DisplayCard playerTwo = new DisplayCard(name2);
-        playerOne.displayPlayerCard();
-        playerTwo.displayPlayerCard();
-
-
-
-        playerOne.checkBox("Green", 7);
-        playerOne.checkBox("Green", 9);
-        playerOne.checkBox("Red", 2);
-
-
-
-        playerTwo.checkBox("Green", 6);
-        playerTwo.checkBox("Green", 11);
-        playerTwo.checkBox("Red", 9);
-
-        playerOne.displayPlayerCard();
-        playerTwo.displayPlayerCard();
     }
 }

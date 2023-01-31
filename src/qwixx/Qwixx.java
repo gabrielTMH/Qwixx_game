edf120ecfc38deec48364ff93ab64088c9f91671
Cristian Panaro <cpanaro@lclark.edu>
2023-01-31 15:19:23 -0800

    added dice.sumWhite() which returns sum of the two white dice. Allow players to check off sum of white dice. Also added function to displayCards in Qwixx.

--- src/qwixx/Qwixx.java
+++ src/qwixx/Qwixx.java
@@ -4,10 +4,12 @@ import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class Qwixx {
+    static LinkedHashMap<String, DisplayCard> players = new LinkedHashMap<>();
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter number of players: ");
-        LinkedHashMap<String, DisplayCard> players = new LinkedHashMap<>();
+        //LinkedHashMap<String, DisplayCard> players = new LinkedHashMap<>();
         int numPlayers = scan.nextInt();
         scan.nextLine(); // ensures we don't add the enter key after the numPlayers as a player
         for (int i = 1; i <= numPlayers; ++i) {
@@ -15,9 +17,25 @@ public class Qwixx {
             String name = scan.nextLine();
             players.put(name, new DisplayCard(name));
         }
-        for (DisplayCard playerCard: players.values()) playerCard.displayPlayerCard();
+        displayCards();
+//        for (DisplayCard playerCard: players.values()) playerCard.displayPlayerCard();
         Die dice = new Die();
         dice.rollAll();
-        dice.rollAll();
+
+        for (String player: players.keySet()) {
+            System.out.println(player + ": would you like to check off " + dice.sumWhite() + "? Type yes/no.");
+            if(scan.nextLine().equals("yes")) {
+                System.out.println("What color would you like to check off?");
+                //add something that makes it non case-sensitive
+                String color = scan.nextLine();
+                players.get(player).checkBox(color, dice.sumWhite());
+            }
+        }
+        displayCards();
+
+    }
+
+    private static void displayCards() {
+        for (DisplayCard playerCard: players.values()) playerCard.displayPlayerCard();
     }
 }

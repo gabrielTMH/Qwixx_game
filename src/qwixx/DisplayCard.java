481841b0b2fd6f720746c7e643573d44cccff117
1dylanangel <1dylanangel@gmail.com>
2023-02-02 14:00:56 -0800

    Changed card map arrays from int to enum

--- src/qwixx/DisplayCard.java
+++ src/qwixx/DisplayCard.java
@@ -1,19 +1,23 @@
 package qwixx;
 import java.util.*;
 
-/*
-    possibly change to enum
- 0 -> unmarked
--1 -> unavailable
- 1 -> checked
-*/
-
 public class DisplayCard {
+    public enum BoxValues {
+        CHECKED,
+        UNAVAILABLE,
+        AVAILABLE;
+        public static BoxValues[] newTrack() {
+            BoxValues[] result = new BoxValues[11];
+            Arrays.fill(result, AVAILABLE);
+            return result;
+        }
+    }
+
+//    private PossibleBoxValues[] boxValues;
 
     //sus on public vs static vs private
-    public LinkedHashMap<String, int[]> trackMap;
-    //penalty not static
-    static char[] penalties= {'_', '_', '_', '_'};
+    public LinkedHashMap<String, Object[]> trackMap;
+    char[] penalties= {'_', '_', '_', '_'};
 
     public String name;
 
@@ -28,65 +32,61 @@ public class DisplayCard {
         System.out.println("\n");
     }
 
-    public static LinkedHashMap<String, int[]> createTrackMap() {
-        LinkedHashMap<String, int[]> playerMap = new LinkedHashMap<>();
-        playerMap.put("Red",new int[11]);
-        playerMap.put("Yellow",new int[11]);
-        playerMap.put("Green",new int[11]);
-        playerMap.put("Blue",new int[11]);
+    public LinkedHashMap<String, Object[]> createTrackMap() {
+        LinkedHashMap<String, Object[]> playerMap = new LinkedHashMap<>();
+        playerMap.put("Red", BoxValues.newTrack());
+        playerMap.put("Yellow", BoxValues.newTrack());
+        playerMap.put("Green", BoxValues.newTrack());
+        playerMap.put("Blue", BoxValues.newTrack());
         return playerMap;
     }
 
-    public static void displayTracks(LinkedHashMap<String, int[]> card) {
+    public static void displayTracks(LinkedHashMap<String, Object[]> card) {
         int count;
         for(String trackColor: card.keySet()) {
             if (trackColor.equals("Red") || trackColor.equals("Yellow")) {
                 System.out.print(trackColor + " ");
                 count = 2;
-                for (int box: card.get(trackColor)){
-                    if (box == 1) System.out.print("X ");
-                    else if (box == -1) System.out.print("- ");
+                for (Object box: card.get(trackColor)){
+                    if (box == BoxValues.CHECKED) System.out.print("X ");
+                    else if (box == BoxValues.UNAVAILABLE) System.out.print("- ");
                     else System.out.print(count + " ");
-                    count++;
+                    ++count;
                 }
                 System.out.print("@\n");
             }
             else {
                 System.out.print(trackColor + " ");
                 count = 12;
-                for (int box : card.get(trackColor)) {
-                    if (box == 1) System.out.print("X ");
-                    else if (box == -1) System.out.print("- ");
+                for (Object box: card.get(trackColor)){
+                    if (box == BoxValues.CHECKED) System.out.print("X ");
+                    else if (box == BoxValues.UNAVAILABLE) System.out.print("- ");
                     else System.out.print(count + " ");
-                    count--;
+                    --count;
                 }
                 System.out.print("@\n");
             }
         }
     }
 
-    public static void displayPenalties() {
+    public void displayPenalties() {
         System.out.print("Penalties ");
         for (char penalty:penalties) System.out.print(penalty + " ");
     }
 
     public void hideUnavailable(String color, int index) {
         for (int i = index; i >= 0 ; i--) {
-            if(this.trackMap.get(color)[i]==0){
-                this.trackMap.get(color)[i] = -1;
+            if(this.trackMap.get(color)[i] == BoxValues.AVAILABLE){
+                this.trackMap.get(color)[i] = BoxValues.UNAVAILABLE;
             }
         }
     }
     public void checkBox(String color, int num){
         int index;
         //Add statement for illegal move (i.g. 13)
-        if(color.equals("Red") || color.equals("Yellow")){
-            index = num-2;
-        }
-        else{
-            index = 12-num;
-        }
-        this.trackMap.get(color)[index] = 1;
+        if(color.equals("Red") || color.equals("Yellow")) index = num - 2;
+        else index = 12 - num;
+        this.trackMap.get(color)[index] = BoxValues.CHECKED;
         hideUnavailable(color, index);
     }
 }

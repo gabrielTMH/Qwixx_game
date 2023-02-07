524f374dc0656697fe389a122e9c94d168e3d3da
Jessica Erickson <jessicaerickson@lclark.edu>
2023-02-07 15:02:11 -0800

    Added Game.java and GameTest

---
+++ test/qwixx/GameTest.java
@@ -0,0 +1,39 @@
+package qwixx;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+    private Game g;
+
+    @BeforeEach
+    void setUp(){
+        g = new Game();
+        g.players.put("p1", new DisplayCard("p1"));
+        g.players.put("p2", new DisplayCard("p2"));
+        g.players.put("p3", new DisplayCard("p3"));
+        g.iterator = g.players.keySet().iterator();
+    }
+
+    @Test
+    void constructorSetsFirstActivePlayer() {
+        g.setActivePlayer();
+        assertEquals("p1", g.activePlayer.toString());
+    }
+
+    @Test
+    void switchesActivePlayer() {
+        g.setActivePlayer();
+        g.setActivePlayer();
+        assertEquals("p2", g.activePlayer.toString());
+    }
+
+    @Test
+    void loopsBackToFirstPlayer() {
+        g.setActivePlayer();
+        g.setActivePlayer();
+        g.setActivePlayer();
+        g.setActivePlayer();
+        assertEquals("p1", g.activePlayer.toString());
+    }
+}
